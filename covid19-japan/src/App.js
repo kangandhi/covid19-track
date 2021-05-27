843357a78c76bdcce17804eb73c18f3c447e7247
kangandhi <81741400+kangandhi@users.noreply.github.com>
2021-05-27 16:58:50 +0900

    tried to modify a infobox area by including both API keys & values

--- covid19-japan/src/App.js
+++ covid19-japan/src/App.js
@@ -9,47 +9,70 @@ import {
 import "./App.css";
 import InfoBox from "./InfoBox";
 import Map from "./Map";
+import Table from "./Table";
+import { sortData } from "./util";
+
+// STATE = how to write a variable in REACT <<<<<<
+//https://covid19-japan-web-api.now.sh/api/v1/prefectures
+//USEEFFECT = run a piece of code based on a given condition (The code inside in USEFFET will run once when the components loads & not again)
+//asyn => send a request, wait for it, do something with
 
 function App() {
   const [prefectures, setPrefectures] = useState([]);
   const [prefecture, setPrefecture] = useState("japan");
   const [prefectureInfo, setPrefectureInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
 
+  // useEffect to fetch data from total japan cases
+  useEffect(() => {
+    fetch("https://covid19-japan-web-api.now.sh/api/v1/total")
+      .then((response) => response.json())
+      .then((data) => {
+        setPrefectureInfo(data);
+      });
+  }, []);
+
+  // useEffect to fetch cases from each prefectures 
   useEffect(() => {
     const getPrefecturesData = async () => {
       await fetch("https://covid19-japan-web-api.now.sh/api/v1/prefectures")
         .then((response) => response.json())
         .then((data) => {
           const prefectures = data.map((prefecture) => ({
-            name: prefecture.name_en, // (Mapped with API data) In english , UnitedState, United Kingdom
-            value: prefecture.id, // (Mapped with API) In japanese
+            name: prefecture.name_en, // (Mapped with API data) In english , hokkaido, aomori
+            value: prefecture.id, // (Mapped with API) with ID
           }));
+          const sortedData = sortData(data);
+          setTableData(sortedData);
+          setTableData(data);
           setPrefectures(prefectures);
         });
     };
     getPrefecturesData();
   }, []);
+
+  // onPrefectureChange event define in return statement 
   const onPrefectureChange = async (event) => {
-    const prefectureNCode = event.target.value;
-    console.log("YOOOOOOO >>>>>>>>", prefectureNCode);
-    setPrefecture(prefectureNCode);
-
+    const prefectureID = event.target.value;
+    // console.log("YOOOOOOO >>>>>>>>", prefectureID);
+    setPrefecture(prefectureID);
     const url =
-      prefectureNCode === "japan"
+      prefectureID === "japan"
         ? "https://covid19-japan-web-api.now.sh/api/v1/total"
-        : `https://covid19-japan-web-api.now.sh/api/v1/prefectures/${prefectureNCode}`;
+        : `https://covid19-japan-web-api.now.sh/api/v1/prefectures/${prefectureID}`;
 
-    //`https://covid19-japan-web-api.now.sh/api/v1/prefectures/${prefectureNCode}`
-
+    //`https://covid19-japan-web-api.now.sh/api/v1/prefectures/${prefectureID}`
     await fetch(url)
       .then((response) => response.json())
       .then((data) => {
-        setPrefecture(prefectureNCode);
+        setPrefecture(prefectureID);
         // All of the data ....  from the prefecture response
         setPrefectureInfo(data);
       });
   };
   console.log("PREFECTURE INFO >>>>>", prefectureInfo);
+
+
 
   return (
     <div className="app">
@@ -62,7 +85,7 @@ function App() {
               onChange={onPrefectureChange}
               value={prefecture}
             >
-              ><MenuItem value="japan">Japan</MenuItem>
+              <MenuItem value="japan">Japan</MenuItem>
               {/* loop through all the prefectures and show a drop down list of the option */}
               {prefectures.map((prefecture) => (
                 <MenuItem value={prefecture.value}>{prefecture.name}</MenuItem>
@@ -78,18 +101,23 @@ function App() {
         <div className="app__stats">
           <InfoBox
             title="Coronavirus Cases"
-            cases={prefectureInfo.cases}
-            total={2000}
+            cases={prefectureInfo.positive} // for all over japan 
+            // cases={prefectureInfo.cases} // for each prefecture
+            //total={2000}
           ></InfoBox>
+
           <InfoBox
             title="Recovered"
-            cases={prefectureInfo.discharge}
-            total={2000}
+            cases={prefectureInfo.discharge} // for all over japan 
+            // cases={prefectureInfo.discharge} // for each prefecture
+            //total={2000}
           ></InfoBox>
+
           <InfoBox
             title="Deaths"
-            cases={prefectureInfo.deaths}
-            total={0}
+            cases={prefectureInfo.death} // for all over japan 
+            // cases={prefectureInfo.deaths} // for each prefecture
+            //total={0}
           ></InfoBox>
         </div>
         {/* Map */}
@@ -99,8 +127,11 @@ function App() {
       <Card className="app__right">
         <CardContent>
           <h3>Live Cases by prefecture</h3>
+          <h4>I am a table</h4>
           {/* Table */}
+          <Table prefectures={tableData}></Table>
           <h3>All over Japan new cases</h3>
+          <h4>I am a graph</h4>
           {/* Graph */}
         </CardContent>
       </Card>
